fe69469a9b86f7c03fcbc9662fa1ec62d0a2f0b5
Jeffer <bogotadiscoverapp@gmail.com>
2025-07-01 08:23:23 -0500

    feat(config) add initial configuration initial)

---
+++ lib/l10n/generated/app_localizations_es.dart
@@ -0,0 +1,19 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Spanish Castilian (`es`).
+class AppLocalizationsEs extends AppLocalizations {
+  AppLocalizationsEs([String locale = 'es']) : super(locale);
+
+  @override
+  String get title => 'Bienvenido';
+
+  @override
+  String get changeLanguage => 'Cambiar idioma';
+
+  @override
+  String get otherButton => 'Other button';
+}
---
+++ lib/src/config/theme/app_colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Color(0xFF1E3A8A);
+  static const secondary = Color(0xFF60A5FA);
+  static const background = Color(0xFFF3F4F6);
+  static const surface = Color(0xFF10B981);
+  static const success = Color(0xFF10B981);
+  static const error = Color(0xFFB00020);
+
+  static const textPrimary = Color(0xFF374151);
+  static const textSecondary = Color(0xFF49454F);
+
+}
